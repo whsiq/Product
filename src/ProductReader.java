810bbca8029d08c8c73c2c18dbfbac76c5b7d14f
whsiq <staggsce@mail.uc.edu>
2023-01-23 12:20:44 -0500

    Second Commit - Completed Program

--- src/ProductReader.java
+++ src/ProductReader.java
@@ -1,5 +1,94 @@
-public class ProductWriter {
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductReader {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Initializations
+
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec;
+        ArrayList<String> records = new ArrayList<>();
+
+        int length = 4;
+        String id;
+        String name;
+        String desc;
+        double cost;
+
+        // Processing
+
+        try {
+
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                int line = 0;
+
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    records.add(rec);
+                    line++;
+                }
+
+                reader.close();
+                System.out.println("\nFile read successfully");
+
+                String[] fields;
+
+                System.out.print("ID#   | Product Name |      Description       |  Cost");
+                System.out.print("\n========================================================");
+                for (String l : records) {
+
+                    fields = l.split(",");
+
+                    if (fields.length == length) {
+                        id = fields[0].trim();
+                        name = fields[1].trim();
+                        desc = fields[2].trim();
+                        cost = Double.parseDouble(fields[3].trim());
+                        System.out.printf("\n%-8s%-15s%-25s%6.2f", id, name, desc, cost);
+                    }
+                    else {
+                        System.out.println("File may be corrupt: ");
+                        System.out.println(l);
+                    }
+                }
+
+            }
+
+            else {
+
+                System.out.println("No file chosen");
+                System.out.println("Please run the program again.");
+                System.exit(0);
+
+            }
+        }
+
+        catch(FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/ProductWriter.java
+++ src/ProductWriter.java
@@ -1,5 +1,61 @@
-public class Main {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductWriter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Initializations
+
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> products = new ArrayList<>();
+        String id;
+        String name;
+        String desc;
+        double cost;
+        boolean ynConfirm = false;
+
+        // Create data for each product
+
+        do {
+
+            id = SafeInput.getRegExString(in, "Enter the product's 6 digit ID", "\\d{6}");
+            name = SafeInput.getNonZeroLenString(in, "Enter the product name");
+            desc = SafeInput.getNonZeroLenString(in, "Enter the product description");
+            cost = SafeInput.getDouble(in, "Enter the product cost");
+            ynConfirm = SafeInput.getYNConfirm(in, "Do you have more entries to make?");
+            products.add(id + ", " + name + ", " + desc + ", " + cost);
+
+        } while(ynConfirm);
+
+        // File Input for each person in the ArrayList
+
+        File workingDirectory = new File(System.getProperty("user.dir")); // Project Directory
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\ProductTestData.txt"); // Select input file
+
+        try {
+
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            for(String rec : products) {
+
+                writer.write(rec, 0, rec.length()); // 0 represents the starting character on the write
+                writer.newLine();
+
+            }
+            writer.close(); // closes the file and flushes buffer
+            System.out.println("Data written to file");
+        }
+        catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
     }
 }
